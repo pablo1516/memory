4a4ed8d5e295e4d4228c4dfa876eba87c90ae93f
pablo1516 <61156436+pablo1516@users.noreply.github.com>
2020-03-03 22:07:22 -0500

    Add files via upload

---
+++ Pares3/src/pares3/Pares3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pares3;
+
+/**
+ *
+ * @author david
+ */
+public class Pares3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmpares FRMPARES = new frmpares();
+        FRMPARES.setVisible(true);
+        FRMPARES.setLocationRelativeTo(null);
+        FRMPARES.setSize(390, 270);
+        FRMPARES.setResizable(false);
+        FRMPARES.setTitle("Memoria");
+    }
+    
+}
